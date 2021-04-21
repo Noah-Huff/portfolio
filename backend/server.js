41dfac339553f2c5346817d8e8d0ead3b4118bd4
Noah-Huff <>
2021-04-21 11:08:18 -0500

    CORS as env variable

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,8 @@ app.use('/api', (req, res, next) => {
     console.log("inside /api for express")
     console.log("Development CORS");
     console.log("Process ", process.env.NODE_ENV);
-    res.header("Access-Control-Allow-Origin", "https://noahhuff.herokuapp.com");
-    if (process.env.NODE_ENV === 'development' && process.env.NODE_ENV !== 'production') {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    }
+    res.header("Access-Control-Allow-Origin", process.env.CORS);
+
 
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
