776cb619712346f9465d7c2124324058f0fb2ba7
Noah-Huff <>
2021-04-21 09:59:29 -0500

    seeing if PORT 5000 does it

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ let PORT = process.env.PORT;
 
 app.use('/api', (req, res, next) => {
     if (process.env.NODE_ENV === 'production') {
-        res.header("Access-Control-Allow-Origin", `http://localhost:${process.env.PORT}`);
+        res.header("Access-Control-Allow-Origin", `http://localhost:5000`);
     } else {
         res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     }
