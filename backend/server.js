40fc7175d16172a92ff910e8c8f3c8a796a7763b
Noah-Huff <>
2021-04-21 11:02:52 -0500

    make sure NODE_ENV is defined

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use('/api', (req, res, next) => {
     console.log("Development CORS");
     console.log("Process ", process.env.NODE_ENV);
     res.header("Access-Control-Allow-Origin", "https://noahhuff.herokuapp.com");
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development' && process.env.NODE_ENV !== undefined) {
         res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     }
 
