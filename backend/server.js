a77c10cb782e4c800a2baa4aebafa185bbc3b430
Noah-Huff <>
2021-04-21 01:05:01 -0500

    changing PORT to 4000

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const sentEmail = require('./functions/sendEmail');
 
 let app = express();
-let PORT = 5000;
+let PORT = 4000;
 
 app.use('/api', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
