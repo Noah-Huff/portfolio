8e4b2edaaeba7bb2829d72221de810fa95226527
Noah-Huff <>
2021-04-21 14:58:40 -0500

    updated profile

--- portfolio-front/src/components/Profile.js
+++ portfolio-front/src/components/Profile.js
@@ -2,30 +2,20 @@ import React from 'react'
 
 const Profile = () => {
     return (
-                <p className="text-card">
-                    Recently, a mentor recommended the Gallups StrengthsFinder
-                    quiz to learn more about applying my strengths. Several of
-                    these strengths are related to adaptability and learning.
-            <br /><br />
-            As an example of adaptability, while working at F’real Foods,
-            I dealt with internal and external customers providing technical
-            solutions. I had to prioritize and schedule which tasks were most
-            critical on a day by day basis. More recently, while participating
-            in a group project for the UMKC certificate program, I set aside
-            my project suggestion to lead the project that the group agreed on.
-            <br /><br />
-            As a life long learner I seek out ways to constantly grow in new
-            areas. At a previous company, I was able to take the initiative
-            and explored the implementation and methods of embedded software.
-            Continuing this interest, I am now working with freeRTOS on an
-            Arduino board to learn more about these concepts. Additionally,
-            I have been developing web applications through the UMKC
-            certificate program using Mongo, Express, Angular and Node.
-            Using this experience, I have built a full stack website! Both
-            of these projects represent different programming paradigms and
-            interests which I’m looking forward to applying in a professional
-            environment.
+        <div className="text-card">
+            <p>
+                I have has a strong desire to study topics in Computer Science. For a while this was an area that I would study intermittently, but recently I’ve been able to devote more energy to this passion. During the Summer of 2020, I had the amazing opportunity to go back to school to start the journey of pursuing a Masters of Science in Computer Science.
             </p>
+            <p>
+                My desire to pursue computer science started much earlier than pursuing my Master’s degree. When I was young, my dad set up an old computer running Windows XP that I could tinker with some basic computer programming. From there, during my undergraduate studies I was able to take electives in programming. Even more recently, while working for a small company I was able to look at the firmware for their equipment and experiment with the equipment.
+            </p>
+            <p>
+                Computers have been a long term interest to me. So when the opportunity came to begin studying for my Computer Science degree, I was very excited. In addition to studying at the University of Central Missouri, I was able to take a bootcamp sponsored by the Full Employment Council and the University of Missouri Kansas City that finished in March 2021. Both of these have been crucial to my beginning understanding of Computer Science topics. Also, check out the projects I have listed on my projects page!
+            </p>
+            <p>
+                Right now, I’m worked the most with React, Express, MongoDB, and a little bit in Node. However, I’ve worked with Angular, I’m studying and getting more familiar with both Java and C#. I’m very excited for this career path and learning as much as possible to be the best developer that I can be!
+            </p>
+        </div>
     )
 }
 
