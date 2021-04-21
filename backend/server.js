4d906b18880be08dcfdca378ea21cb626c232ee8
Noah-Huff <>
2021-04-21 01:12:35 -0500

    changing cors header

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ let app = express();
 let PORT = process.env.PORT;
 
 app.use('/api', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    if (process.env.NODE_ENV === 'production') {
+        res.header("Access-Control-Allow-Origin", `http://localhost:${process.env.NODE_ENV}`);
+    } else {
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
     next();
