dfe103199b4f36d39432a129f33f58262e181787
Noah-Huff <>
2021-04-21 15:47:24 -0500

    another profile edit

--- portfolio-front/src/components/Profile.js
+++ portfolio-front/src/components/Profile.js
@@ -4,17 +4,17 @@ const Profile = () => {
     return (
         <div className="text-card">
             <p>
-                I have has a strong desire to study topics in Computer Science. For a while this was an area that I would study intermittently, but recently I’ve been able to devote more energy to this passion. During the Summer of 2020, I had the amazing opportunity to go back to school to start the journey of pursuing a Masters of Science in Computer Science.
-            </p>
+                I have a strong desire to study topics in Computer Science which started at an early age. For a while this was an area that I would study intermittently, but recently I’ve been able to devote more energy to this passion. During the Summer of 2020, I had the amazing opportunity to go back to school to start the journey of pursuing a Masters of Science in Computer Science.
+</p>
             <p>
                 My desire to pursue computer science started much earlier than pursuing my Master’s degree. When I was young, my dad set up an old computer running Windows XP that I could tinker with some basic computer programming. From there, during my undergraduate studies I was able to take electives in programming. Even more recently, while working for a small company I was able to look at the firmware for their equipment and experiment with the equipment.
-            </p>
+</p>
             <p>
                 Computers have been a long term interest to me. So when the opportunity came to begin studying for my Computer Science degree, I was very excited. In addition to studying at the University of Central Missouri, I was able to take a bootcamp sponsored by the Full Employment Council and the University of Missouri Kansas City that finished in March 2021. Both of these have been crucial to my beginning understanding of Computer Science topics. Also, check out the projects I have listed on my projects page!
-            </p>
+</p>
             <p>
                 Right now, I’m worked the most with React, Express, MongoDB, and a little bit in Node. However, I’ve worked with Angular, I’m studying and getting more familiar with both Java and C#. I’m very excited for this career path and learning as much as possible to be the best developer that I can be!
-            </p>
+</p>
         </div>
     )
 }
